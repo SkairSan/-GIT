87b7bf80b4e1e14be1f2fcce107c2550e59d1f89
SkairSan <vlad.muckob@mail.ru>
2026-03-08 12:33:03 +0000

    Update lw2array_2.php

--- lw2array_2.php
+++ lw2array_2.php
@@ -7,17 +7,15 @@ $matrix = [
 ];
 
 $sum = 0;
-$size = count($matrix); // Определяем размер матрицы
+$size = count($matrix);
 
 for ($i = 0; $i < $size; $i++) {
-    // Главная диагональ — это элементы [0][0], [1][1], [2][2]...
     $sum += $matrix[$i][$i];
 }
 
-echo "Матрица:\n";
 foreach ($matrix as $row) {
     echo implode("\t", $row) . "\n";
 }
 
-echo "\nСумма элементов главной диагонали: " . $sum;
+echo "\nSum: " . $sum;
 ?>
